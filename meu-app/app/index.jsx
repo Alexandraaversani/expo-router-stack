ab6cdb2672115f8987260ff6ff96a9982f04318d
Alexandraaversani <158167102+Alexandraaversani@users.noreply.github.com>
2025-03-27 10:29:13 -0300

    feat: create button

--- meu-app/app/index.jsx
+++ meu-app/app/index.jsx
@@ -1,16 +1,49 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Link, Stack, useNavigation} from 'expo-router';
 
-export default function Screen() {
+export default function Home() {
+    const navigation = useNavigation();
+
+    const handHideHeader = () => {
+        navigation.setOptions({headerShown: false,});
+    }
+    const handOffHeader = () => {
+        navigation.setOptions({headerShown: true,});
+    }
     return (
-        <View style={styles.container}>
+
+        <View style={styles.container}> 
+            <Stack.Screen options={{
+                title: 'Senai',
+                headerRight: () => (
+                    <Button 
+                    onPress={() => alert('Olá, Aula de Mobile!')}
+                    title="Info"
+                    color='#0000'
+                    />
+                ),
+                
+                headerStyle: {
+                    backgroundColor: '#FFD700',
+                },
+                headerTintColor: '#000',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 30,
+                }
+            }}/>
+
+            <Button title="Ocultar Header" onPress={handHideHeader} />
+            <Button title="Ativar Header" onPress={handOffHeader} />
+
             <Text>Bem-vindo à tela inicial!</Text>
             <Link href="/sobre">Ir para Sobre</Link>
         </View>
+        
     );
 }
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
